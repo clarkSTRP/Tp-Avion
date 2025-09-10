f33e7a96322b879cf33a970fe96c7882469d6dc9
Yelodress <yelodress@pm.me>
2025-09-10 16:54:21 +0200

    added all GET methods

--- backend/app.py
+++ backend/app.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Tp-Avion"})
 
+# Routes des compagnies
 @app.get("/compagnies")
 def list_compagnies():
     with engine.connect() as conn:
@@ -33,10 +34,42 @@ def get_compagnie_code(ref):
         rows = [dict(r) for r in result.mappings()]
     return jsonify(rows)
 
-@app.get("/aeroports")
+# Routes des aeroports
+@app.get("/aeroports") # Afficher la liste des aéroports
 def list_aeroports():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT id, nom, code, ville FROM aeroport"))
+        rows = [dict(r) for r in result.mappings()]
+    return jsonify(rows)
+
+# Routes des vols
+@app.get("/vols") #Afficher tous les vols disponibles
+def list_vols():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT id, aeroport_depart_id, aeroport_arrivee_id, prix, places_disponibles FROM vol"))
+        rows = [dict(r) for r in result.mappings()]
+    return jsonify(rows)
+
+@app.get("/vols/<ref>") # Afficher détail d'un vol
+def get_vols_code(ref):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM vol Where LOWER(numero_vol) = LOWER(:ref)"),{"ref": ref})
+        rows = [dict(r) for r in result.mappings()]
+    return jsonify(rows)
+
+# Route des passagers
+@app.get("/passager/<ref>") # Afficher détail d'un passager
+def get_passager_code(ref):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM passager Where LOWER(passport_numero) = LOWER(:ref)"),{"ref": ref})
+        rows = [dict(r) for r in result.mappings()]
+    return jsonify(rows)
+
+# Route de réservations
+@app.get("/reservations") #Afficher tous les vols disponibles
+def list_reservations():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT id, passager_id, vol_id, status, date_reservation FROM reservation"))
         rows = [dict(r) for r in result.mappings()]
     return jsonify(rows)
 
