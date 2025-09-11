9da948bfb4382e342d521774c53896ac8d86a556
Yelodress <yelodress@pm.me>
2025-09-11 15:06:21 +0200

    Added numero_vol when requesting w/ POST /vols

--- backend/app/routes/vols.py
+++ backend/app/routes/vols.py
@@ -7,14 +7,14 @@ def vols_endpoint(app):
     @app.get("/vols") #Afficher tous les vols disponibles
     def list_vols():
         with engine.connect() as conn:
-            result = conn.execute(text("SELECT id, aeroport_depart_id, aeroport_arrivee_id, prix, places_disponibles FROM vol"))
+            result = conn.execute(text("SELECT id, numero_vol, aeroport_depart_id, aeroport_arrivee_id, prix, places_disponibles FROM vol"))
             rows = [dict(r) for r in result.mappings()]
         return jsonify(rows)
     
     @app.get("/vols/<ref>") # Afficher détail d'un vol
     def get_vols_code(ref):
         with engine.connect() as conn:
-            result = conn.execute(text("SELECT * FROM vol Where LOWER(numero_vol) = LOWER(:ref)"),{"ref": ref})
+            result = conn.execute(text("SELECT numero_vol, compagnie_id, aeroport_depart_id, aeroport_arrivee_id, heure_depart, heure_arrivee, prix, places_disponibles FROM vol Where LOWER(numero_vol) = LOWER(:ref)"),{"ref": ref})
             rows = [dict(r) for r in result.mappings()]
         return jsonify(rows)
 
